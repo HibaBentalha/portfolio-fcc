41c4a1af0440714655ed3df915d2dc77dd3ac36f
Hiba <bentalha.hiba@gmail.com>
2017-11-28 20:17:03 +0100

    upload porfolio files
    first portfolio

---
+++ fcc2.js
@@ -0,0 +1,9 @@
+document.onscroll = menuFixed;
+function menuFixed()
+{
+	if(window.scrollY > 0){    
+    document.querySelector('nav').classList.add('menu-fixed');
+    }
+	else	{document.querySelector('nav').classList.remove('menu-fixed');
+    }
+}
